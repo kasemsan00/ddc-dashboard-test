c50e3efc08a8782f072591a8f7336c2f6a2b8ac6
kasemsan00 <106854077+kasemsan00@users.noreply.github.com>
2023-05-22 10:27:54 +0700

    dev

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -5,12 +5,7 @@ import { MENU_ITEMS } from './pages-menu';
 @Component({
   selector: 'd1442-dashboard-pages',
   styleUrls: ['pages.component.scss'],
-  template: `
-    <ngx-one-column-layout>
-      <nb-menu [items]="menu"></nb-menu>
-      <router-outlet></router-outlet>
-    </ngx-one-column-layout>
-  `,
+  template: `<div>Hello</div>`,
 })
 export class PagesComponent {
   menu = MENU_ITEMS;
